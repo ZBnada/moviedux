1c1a253f3d99a72a6377b0257e10624957adcdf0
ZBnada <nadazouabi5@gmail.com>
2025-08-02 18:36:46 +0200

    crate a movie use state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './styles.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import MoviesGrid from './component/MoviesGrid';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Header />
+        <MoviesGrid></MoviesGrid>
       </div>
       <Footer />
     </div>
